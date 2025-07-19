2034532cb6bee61e007b01b454d1635dc712bbec
klio-24 <84352530+klio-24@users.noreply.github.com>
2025-07-19 16:59:53 +0100

    python script to upload images based on prompt to s3 working

--- redis_vm/runpod_worker.py
+++ redis_vm/runpod_worker.py
@@ -1,6 +1,7 @@
 from diffusers import StableDiffusionXLPipeline
 import torch
 import boto3
+from io import BytesIO # this allows straight upload using a buffer instead of saving (saves space overall)
 
 pipe = StableDiffusionXLPipeline.from_pretrained(
     "stabilityai/stable-diffusion-xl-base-1.0",  # This downloads SDXL
@@ -12,9 +13,16 @@ pipe = StableDiffusionXLPipeline.from_pretrained(
 prompt = "a futuristic city skyline at night, high detail, cinematic"
 image = pipe(prompt).images[0]
 
+buffer = BytesIO()
+image.save(buffer, format="PNG")
+buffer.seek(0) # resets pointer of buffer back to start so upload_file reads the buffer correctly
+
 
 s3 = boto3.client("s3")
 bucket_name = "rollerai-generated-images"            
-s3_key = "uploads/output.png"                    
 
-s3.upload_file("output.png", bucket_name, s3_key)
+key = "output.png"
+s3.upload_fileobj(buffer, bucket_name, key)
+
+
+
