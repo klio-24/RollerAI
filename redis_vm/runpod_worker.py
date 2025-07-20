26e6024ab17b41eacfdfe7d2082f02710179f9bd
klio-24 <84352530+klio-24@users.noreply.github.com>
2025-07-20 18:26:37 +0100

    s3 uploads of images from a frontend message now working

--- redis_vm/runpod_worker.py
+++ redis_vm/runpod_worker.py
@@ -1,25 +1,23 @@
-from diffusers import StableDiffusionXLPipeline
+from diffusers import StableDiffusionPipeline
 import torch
 import boto3
 import redis
 from io import BytesIO # this allows straight upload using a buffer instead of saving (saves space overall)
 from redis import Redis
-from rq import Worker,Queue
+from rq import Worker,Queue,SimpleWorker
 
 r = redis.Redis(host="18.132.136.177", port=6379, db=0)
 
-pipe = StableDiffusionXLPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-xl-base-1.0",  # This downloads SDXL
+pipe = StableDiffusionPipeline.from_pretrained(
+    "runwayml/stable-diffusion-v1-5",  # This downloads SDXL
     torch_dtype=torch.float16,
-    variant="fp16",
     use_safetensors=True
 ).to("cuda")
 
-s3 = boto3.client("s3")  
+s3 = boto3.client("s3")
 bucket_name = "rollerai-generated-images"
 
 def generate_image_rq(job_id: str, prompt: str):
-    global pipe
     image = pipe(prompt).images[0]
     buffer = BytesIO()
     image.save(buffer, format="PNG")
@@ -34,9 +32,9 @@ def generate_image_rq(job_id: str, prompt: str):
         "s3_url": s3_url
     })
 
-    return 
+    return
 
 if __name__ == "__main__": # ensures code only runs if this script is run directly
     queue = Queue('default', connection = r)
-    worker = Worker(queues = [queue],connection = r)
+    worker = SimpleWorker(queues = [queue],connection = r)
     worker.work() # starts the work loop which makes the pod a worker on the queue, connecting to redis
