d5f7d00d2721a347a60ac0b806e9b816e7b9e5df
klio-24 <84352530+klio-24@users.noreply.github.com>
2025-08-09 13:43:51 +0100

    attempting to fix credentials error

--- redis_vm/startup.sh
+++ redis_vm/startup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd /workspace/RollerAI && git pull origin main
 
+
+export AWS_SHARED_CREDENTIALS_FILE=/workspace/.aws/credentials
+export AWS_CONFIG_FILE=/workspace/.aws/config
+
 export PATH=/workspace/.local/bin:$PATH
 source /workspace/venv/bin/activate
 
